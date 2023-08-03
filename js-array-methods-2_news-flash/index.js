331231d65252559c4b9fe5a9809745fb909ea0f7
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-03 12:43:47 +0200

    finish 3rd challenge

---
+++ js-array-methods-2_news-flash/index.js
@@ -0,0 +1,27 @@
+import { Card } from "./components/Card/Card.js";
+import { news } from "./utils/news.js";
+import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+
+// Part 1 - start here
+const filteredNews = news.filter((article) =>
+  article.categories.includes("politics")
+);
+
+// Part 2 - start here
+const sortedNews = filteredNews;
+
+sortedNews.sort(
+  (articleA, articleB) => articleA.body.length - articleB.body.length
+);
+
+sortedNews.forEach((news) => {
+  const cardElement = Card(news);
+  container.append(cardElement);
+});
+
+// Check your filter and sorting order here:
+checkFilteredNews(filteredNews);
+
+checkSortedNews(sortedNews);
