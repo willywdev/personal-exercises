e3de676b3c06e5ec164cf42ae418ed21bdef4323
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-10 11:56:34 +0200

    finish last challenge

---
+++ js-unit-testing_people/index.js
@@ -0,0 +1,50 @@
+export function getFirstNames(people) {
+  return people.map((person) => person.firstName);
+}
+
+export function getFullNames(people) {
+  return people.map((person) => {
+    return `${person.firstName} ${person.lastName}`;
+  });
+}
+
+export function getNameAndAge(people) {
+  return people.map((person) => {
+    return `${person.lastName} (${person.age})`;
+  });
+}
+
+export function getPeopleByAge(people) {
+  return people.slice().sort((personA, personB) => {
+    return personA.age - personB.age;
+  });
+}
+
+export function getPeopleNamesOlderThan(people, age) {
+  return people
+    .filter((person) => person.age > age)
+    .map((person) => `${person.firstName} ${person.lastName}`);
+}
+
+export function getPeopleByLastName(people, lastName) {
+  return people
+    .filter((person) => person.lastName === lastName)
+    .map((person) => `${person.firstName} ${person.lastName}`);
+}
+
+export function findPersonById(people, id) {
+  return people.find((person) => {
+    return person.id === id;
+  });
+}
+
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age > age);
+}
+
+export function getFullNamesSortedByAge(people) {
+  return people
+    .slice()
+    .sort((personA, personB) => personA.age - personB.age)
+    .map((person) => person.lastName);
+}
