e73fbb309872eba398bb5c8125e5e57b0cb65a38
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-16 11:48:23 +0200

    finish second challenge

---
+++ react-basics_hello-world-article/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>Welcome to React!</p>
+    </article>
+  );
+}
