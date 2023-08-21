fdf1662da0cfb4e03fe6d982a2aabb4decb0dc40
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-21 10:19:53 +0200

    finish second challenge

---
+++ react-nesting_fragments/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Box from "./components/Box";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main className="flex-container">
+      <Boxes />
+    </main>
+  );
+}
+
+function Boxes() {
+  return (
+    <>
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </>
+  );
+}
