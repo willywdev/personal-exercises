ce8b69676a34120dab2dbacb2061a934e379e5c1
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-22 11:00:14 +0200

    finish third challenge

---
+++ react-state_emoji-checker/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [code, setCode] = useState("");
+
+  const validCode = "🐡🐠🐋";
+
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={() => {
+            setCode(code + event.target.textContent);
+          }}
+        >
+          <span role="img" aria-label="Pufferfish">
+            🐡
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setCode(code + event.target.textContent);
+          }}
+        >
+          <span role="img" aria-label="Whale">
+            🐋
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setCode(code + event.target.textContent);
+          }}
+        >
+          <span role="img" aria-label="Clownfish">
+            🐠
+          </span>
+        </button>{" "}
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCode("");
+        }}
+      >
+        Reset
+      </button>
+
+      {code === validCode && <p>Valid code!</p>}
+    </div>
+  );
+}
