92efd064af5ebfba80665b7ae3c87923ab7eba2a
Wilhelm Werwein <werwein.dev@gmail.com>
2023-08-29 10:59:55 +0200

    finish last challenge for today

---
+++ react-with-local-storage_dice/src/App.js
@@ -0,0 +1,25 @@
+import useLocalStorageState from "use-local-storage-state";
+import { D6Button } from "./components/D6Button";
+import History from "./components/History";
+import "./styles.css";
+import { getD6Roll } from "./utils";
+
+export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+  const currentRollValue = rolls[0]?.value;
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+  return (
+    <div className="app">
+      <main className="app__main">
+        <D6Button value={currentRollValue} onRoll={handleRoll} />
+      </main>
+      <aside className="app__aside">
+        <History rolls={rolls} />
+      </aside>
+    </div>
+  );
+}
