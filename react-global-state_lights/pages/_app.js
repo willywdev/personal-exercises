177623ad59660522105fc6f38531158a3159778b
Wilhelm Werwein <werwein.dev@gmail.com>
2023-09-08 12:27:00 +0200

    small change

--- react-global-state_lights/pages/_app.js
+++ react-global-state_lights/pages/_app.js
@@ -6,42 +6,34 @@ const allLights = [
   {
     name: "Living Room",
     isOn: false,
-    id: 1,
   },
   {
     name: "Kitchen",
     isOn: false,
-    id: 2,
   },
   {
     name: "Bedroom",
     isOn: false,
-    id: 3,
   },
   {
     name: "Bathroom",
     isOn: false,
-    id: 4,
   },
   {
     name: "Garage",
     isOn: false,
-    id: 5,
   },
   {
     name: "Porch",
     isOn: false,
-    id: 6,
   },
   {
     name: "Garden",
     isOn: false,
-    id: 7,
   },
   {
     name: "Office",
     isOn: false,
-    id: 8,
   },
 ];
 
